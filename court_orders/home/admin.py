0435c94d0e5899db3d5b4e7302bdcbbce02936f2
Incognitosin01 <ssmahajan@mitaoe.ac.in>
2022-03-12 17:50:01 +0530

    all required tables added

--- court_orders/home/admin.py
+++ court_orders/home/admin.py
@@ -1,10 +1,6 @@
+from pydoc import cli
 from django.contrib import admin
 from .models import *
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-class custome_user_admin(UserAdmin):
-    pass
-admin.site.register(custom_user,custome_user_admin)
-admin.site.register(judge)
-admin.site.register(clerk)
-admin.site.register(lawyer)
+admin.site.register([custom_user,judge,clerk,client,criminal,civil,court])
--- court_orders/home/models.py
+++ court_orders/home/models.py
@@ -59,3 +59,84 @@ class lawyer(models.Model):
 
     def __str__(self) -> str:
         return self.reg_no +"---Verification--->"+ str(self.is_verified)
+
+class client(models.Model):
+    aadhar_no =models.CharField(max_length=20,null=True,blank=True)
+    aadhar = models.FileField(upload_to="aadhar/",null=True,blank=True)
+
+class court(models.Model):
+    name = models.CharField(max_length=2000,null=True,blank=True)
+    location = models.CharField(max_length=1000,null=True,blank=True)
+    contact = models.IntegerField(blank=True,null=True)
+    bar_association = models.CharField(max_length=300,null=True,blank=True)
+
+
+class civil(models.Model):
+
+    national_security = '0'
+    state_security = '1'
+    institute_or_govt_officials = '2'
+    women = '3'
+    judge_pref = '4'
+    default = '5'
+
+    priority_level = (
+        (national_security,"nantional_security"),
+        (state_security,'state_security'),
+        (institute_or_govt_officials,'institute_or_govt_officials'),
+        (women,'women'),
+        (judge_pref,'judge_pref'),
+        (default,'default')
+
+    )
+    crn = models.CharField(max_length=500,null=True,blank=True)
+    name  = models.CharField(max_length=500,null=True,blank=True)
+    type = models.CharField(max_length=500,null=True,blank=True)
+    info= models.CharField(max_length=500,null=True,blank=True)
+    enroll_date = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    petitioner= models.CharField(max_length=500,null=True,blank=True)
+    plawyer= models.CharField(max_length=500,null=True,blank=True)
+    respondent= models.CharField(max_length=500,null=True,blank=True)
+    rlawyer= models.CharField(max_length=500,null=True,blank=True)
+    proof = models.FileField(upload_to='proof/',blank=True,null=True)
+    priority = models.CharField(max_length=1,choices=priority_level,default = '5',blank=True,null=True)
+    status = models.CharField(max_length=200,blank=True,null=True)
+    hearing_date = models.DateField(auto_now_add=True,blank=True,null=True)
+    court_id = models.CharField(max_length=16,blank=True,null=True)
+
+class criminal(models.Model):
+    national_security = '0'
+    state_security = '1'
+    institute_or_govt_officials = '2'
+    women = '3'
+    judge_pref = '4'
+    default = '5'
+
+    priority_level = (
+        (national_security,"nantional_security"),
+        (state_security,'state_security'),
+        (institute_or_govt_officials,'institute_or_govt_officials'),
+        (women,'women'),
+        (judge_pref,'judge_pref'),
+        (default,'default')
+
+    )
+    crn = models.CharField(max_length=500,null=True,blank=True)
+    name  = models.CharField(max_length=500,null=True,blank=True)
+    type = models.CharField(max_length=500,null=True,blank=True)
+    info= models.CharField(max_length=500,null=True,blank=True)
+    enroll_date = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    petitioner= models.CharField(max_length=500,null=True,blank=True)
+    plawyer= models.CharField(max_length=500,null=True,blank=True)
+    respondent= models.CharField(max_length=500,null=True,blank=True)
+    rlawyer= models.CharField(max_length=500,null=True,blank=True)
+    proof = models.FileField(upload_to='proof/',blank=True,null=True)
+    priority = models.CharField(max_length=1,choices=priority_level,default = '5',blank=True,null=True)
+    status = models.CharField(max_length=200,blank=True,null=True)
+    hearing_date = models.DateField(auto_now_add=True,blank=True,null=True)
+    court_id = models.CharField(max_length=16,blank=True,null=True)
+    fir_no = models.CharField(max_length=14,blank=True,null=True)
+
+
+
+
