a267f4cc6882663cd02fc10f2faf1ded3e2d26e5
Incognitosin01 <ssmahajan@mitaoe.ac.in>
2022-03-12 22:42:58 +0530

    video chat section added

--- court_orders/court_orders/settings.py
+++ court_orders/court_orders/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'court_orders.wsgi.application'
+ASGI_APPLICATION = 'court_orders.asgi.application'
+
 
 
 # Database
@@ -117,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
